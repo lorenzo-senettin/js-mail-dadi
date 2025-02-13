db50d849104769122bfa03219792cc7a44ac0e51
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-13 23:17:42 +0100

    add cycle to search mail

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ stampa un messaggio appropriato sull’esito del controllo.
 Lo svolgimento va fatto usando un ciclo for è non usando metodi degli array (come includes, per esempio). */
 
 // mail list
-const invitati = ["mario@gmx.com", "lucia@gmx.com", "giuseppe@gmx.com", "anna@gmx.com"];
+const invited = ["mario@gmx.com", "lucia@gmx.com", "giuseppe@gmx.com", "anna@gmx.com"];
 
 // ask user mail
 const user_mail = prompt("Inserisci la tua email per verificare l'accesso alla festa:");
 
+// access denied variable
+let access = false
+
+// for to search mail in the list
+for (i = 0; i < invited.length; i++) {
+  if (invited[i] === user_mail) {
+    access = true;
+    break; // break cycle
+  }
+}
