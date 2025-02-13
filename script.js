75ebea8c2c00990d78a401d685d83c5e585168d3
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-13 23:10:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Lo svolgimento va fatto usando un ciclo for è non usando metodi degli array (come includes, per esempio). */
+
+// mail list
+const invitati = ["mario@gmx.com", "lucia@gmx.com", "giuseppe@gmx.com", "anna@gmx.com"];
+
+// ask user mail
+const user_mail = prompt("Inserisci la tua email per verificare l'accesso alla festa:");
+
